00abe1ca86d4c0c0fa2388f59db9e51811b27d39
Ko-GyeongTae <kokt0203@naver.com>
2022-07-07 09:19:15 +0900

    Chore: config

---
+++ src/config/config.ts
@@ -0,0 +1,40 @@
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "./data.source";
+
+dotenv.config();
+
+export abstract class ConfigServer {
+  constructor() {
+    const nodeNameEnv = this.createPathEnv(this.nodeEnv);
+    dotenv.config({
+      path: nodeNameEnv,
+    });
+  }
+
+  public getEnvironment(k: string): string | undefined {
+    return process.env[k];
+  }
+
+  public getNumberEnv(k: string): number {
+    return Number(this.getEnvironment(k));
+  }
+
+  public get nodeEnv(): string {
+    return this.getEnvironment("NODE_ENV")?.trim() || "";
+  }
+
+  public createPathEnv(path: string): string {
+    const arrEnv: Array<string> = ["env"]; //['hola', 'mundo'] => 'hola.mundo'
+
+    if (path.length > 0) {
+      const stringToArray = path.split(".");
+      arrEnv.unshift(...stringToArray);
+    }
+    return "." + arrEnv.join(".");
+  }
+
+  get initConnect(): Promise<DataSource> {
+    return AppDataSource.initialize();
+  }
+}
